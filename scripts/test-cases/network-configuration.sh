983160d3b7b92c497e681c05c0af4c9e920fbe87
Erik Peyronson <eripe320@student.liu.se>
2016-12-19 11:00:54 +0100

    asd

--- scripts/test-cases/network-configuration.sh
+++ scripts/test-cases/network-configuration.sh
@@ -7,6 +7,16 @@
 # Procedure:
 #!/bin/bash
 
+printf="Testing that HOSTNAME change was permanent \n\n"
+HOSTNAME=hostname
+
+
+if [[ $HOSTNAME -eq "client-1" OR $HOSTNAME -eq "client-2" OR $HOSTNAME -eq "server" OR $HOSTNAME -eq "gw"  ]] ; then
+    printf "ERROR ping -1 -n www.google.com failed" 
+else
+    printf "ping -1 -n www.google.com sucessful: name resolution okay\n"
+fi
+
 printf="Testing name resolution:\nping -c 1 -n www.google.se | grep '0% packet loss'\n"
 
 PING=$(ping -c 1 -n www.google.se | grep '0% packet loss')
@@ -16,26 +26,26 @@ if [[ -z $PING ]] ; then
 else
     printf "ping -1 -n www.google.com sucessful: name resolution okay\n"
 fi
+ 
+# printf="Testing prefix accepting\nroute | grep eth1\n"
 
-printf="Testing prefix accepting\nroute | grep eth1\n"
+# PREFIXES=$(route | grep eth1)
 
-PREFIXES=$(route | grep eth1)
+# if [[ -z $PREFIXES ]]; then
+#     printf "ERROR: no network prefixes found in routing table"
+# else
+#     printf "Network prefixes accepted:\n$PREFIXES"
+# fi
 
-if [[ -z $PREFIXES ]]; then
-    printf "ERROR: no network prefixes found in routing table"
-else
-    printf "Network prefixes accepted:\n$PREFIXES"
-fi
+# printf="Testing prefix distributionn\nroute | grep eth0 |  grep '*'\n"
 
-printf="Testing prefix distributionn\nroute | grep eth0 |  grep '*'\n"
+# PREFIXES=$(route | grep eth0 |  grep '*')
 
-PREFIXES=$(route | grep eth0 |  grep '*')
-
-if [[ -z $PREFIXES ]]; then
-    printf "ERROR: network prefix not distributed\n"
-else
-    printf "Sucess:\n$PREFIXES "
-fi
+# if [[ -z $PREFIXES ]]; then
+#     printf "ERROR: network prefix not distributed\n"
+# else
+#     printf "Sucess:\n$PREFIXES "
+# fi
 
 
 # Expected results:
