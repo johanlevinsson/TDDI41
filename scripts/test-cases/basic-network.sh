b17f5d624230ad21ab5859fa59971dad15e29172
Erik Peyronson <erikpeyronson@gmail.com>
2016-12-26 11:48:34 +0100

    test

--- scripts/test-cases/basic-network.sh
+++ scripts/test-cases/basic-network.sh
@@ -7,22 +7,6 @@
 # Procedure:
 #!/bin/bash
 
-function print_test {
-    TEST_NAME=$1
-    TEST_RESULT=$2    
-    padlength=40
-    pad=$(printf '%0.1s' "-"{1..60})
-    if [ TEST_RESULT == "okey" ]; then
-	printf '%s' "$TEST_NAME"
-	printf '%*.*s' 0 $((padlength - ${#TEST_NAME} )) "$pad"
-	printf "[\e[1m\e[92mOK\e[0m]$okey_string"  
-    else
-	printf '%s' "$TEST_NAME"
-	printf '%*.*s' 0 $((padlength - ${#TEST_NAME} )) "$pad"
-	printf "[\e[1m\e[31mFAIL\e[0m]"
-    fi
-}
-
 # Test hostname and ip: compare hostname with ip
 
 HOSTNAME=$(hostname)
@@ -33,18 +17,18 @@ if [ IP_ADDRESS == "130.236.178.219" ]; then
     if [ HOSTNAME == "server" ]; then
 	EXTERNAL_ADRESS=$(ifconfig | awk '/inet addr/{print substr($2,6)}')
 	if [ EXTERNAL_ADDRESS == "130.236.178.25" ]; then
-	    TEST_RESULT"=okey"
+	    TEST_RESULT=okey
 	else
-	    TEST_result="fail"
+	    TEST_result=fail
 	fi
     elif [ IP_ADDRESS == "130.236.178.218" && HOSTNAME == "gw" ]; then
-	TEST_RESULT="okey"
+	TEST_RESULT=okey
     elif [ IP_ADDRESS == "130.236.178.220" && HOSTNAME == "client_1" ]; then
-	TEST_RESULT="okey"
+	TEST_RESULT=okey
     elif [ IP_ADDRESS == "130.236.178.221" && HOSTNAME == "client_2" ]; then
-	TEST_RESULT="okey"
+	TEST_RESULT=okey
     else
-	TEST_RESULT="Fail"
+	TEST_RESULT=Fail
 	printf "Hostname ip addres on $HOSTNAME failed due to hostname-ip missmatch\n"
     fi
 
@@ -58,9 +42,9 @@ print_test "Basic network" $TEST_RESULT
 
 PING=$(ping -c 1 -n www.google.se | grep '0% packet loss')
 if [[ -z $PING ]] ; then
-    TEST_RESULT="fail"
+    TEST_RESULT=fail
 else
-    TEST_RESULT="okey"
+    TEST_RESULT=okey
 fi
 #exec 1>&3 2>&4
 echo >&2
