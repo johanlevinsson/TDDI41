ef2f78952ef1193e63a8aa13cb19e0fb05558032
Erik Peyronson <erikpeyronson@gmail.com>
2016-12-26 20:13:38 +0100

    moved function

--- scripts/test-cases/basic-network.sh
+++ scripts/test-cases/basic-network.sh
@@ -10,21 +10,21 @@
 #!/bin/bash
 
 
-function print_test {
-    TEST_NAME=$1
-    RESULT=$2    
-    padlength=40
-    pad=$(printf '%0.1s' "-"{1..60})
-    if [ "$RESULT" == "okey" ]; then
-        printf '%s' "$TEST_NAME"
-        printf '%*.*s' 0 $((padlength - ${#TEST_NAME} )) "$pad"
-        printf "[\e[1m\e[92mOK\e[0m]"  
-    else
-        printf '%s' "$TEST_NAME"
-        printf '%*.*s' 0 $((padlength - ${#TEST_NAME} )) "$pad"
-        printf "[\e[1m\e[31mFAIL\e[0m]"
-    fi
-}
+# function print_test {
+#     TEST_NAME=$1
+#     RESULT=$2    
+#     padlength=40
+#     pad=$(printf '%0.1s' "-"{1..60})
+#     if [ "$RESULT" == "okey" ]; then
+#         printf '%s' "$TEST_NAME"
+#         printf '%*.*s' 0 $((padlength - ${#TEST_NAME} )) "$pad"
+#         printf "[\e[1m\e[92mOK\e[0m]"  
+#     else
+#         printf '%s' "$TEST_NAME"
+#         printf '%*.*s' 0 $((padlength - ${#TEST_NAME} )) "$pad"
+#         printf "[\e[1m\e[31mFAIL\e[0m]"
+#     fi
+# }
 
 
 # Test hostname and ip: compare hostname with ip
