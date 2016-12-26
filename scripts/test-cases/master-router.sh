bb8e429779e99261d150870e941b46558e03f38e
Erik Peyronson <erikpeyronson@gmail.com>
2016-12-26 20:15:20 +0100

    moved function

--- scripts/test-cases/master-router.sh
+++ scripts/test-cases/master-router.sh
@@ -12,6 +12,22 @@
 # exec 1>&3 2>&4
 # echo >&2
 
+function print_test {
+    TEST_NAME=$1
+    RESULT=$2    
+    padlength=40
+    pad=$(printf '%0.1s' "-"{1..60})
+    if [ "$RESULT" == "okey" ]; then
+        printf '%s' "$TEST_NAME"
+        printf '%*.*s' 0 $((padlength - ${#TEST_NAME} )) "$pad"
+        printf "[\e[1m\e[92mOK\e[0m]"  
+    else
+        printf '%s' "$TEST_NAME"
+        printf '%*.*s' 0 $((padlength - ${#TEST_NAME} )) "$pad"
+        printf "[\e[1m\e[31mFAIL\e[0m]"
+    fi
+}
+
 source /root/TDDI41/scripts/test-cases/basic-network.sh
 source /root/TDDI41/scripts/test-cases/quagga.sh
 source /root/TDDI41/scripts/test-cases/ntp-client.sh
