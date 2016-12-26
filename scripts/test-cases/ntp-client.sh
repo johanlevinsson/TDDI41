799ccaf64fad0e4323ca33e1f1701fe21febac81
Erik Peyronson <erikpeyronson@gmail.com>
2016-12-26 20:19:43 +0100

    AS!

--- scripts/test-cases/ntp-client.sh
+++ scripts/test-cases/ntp-client.sh
@@ -6,21 +6,21 @@
 
 # Procedure:
 
-function print_test {
-    TEST_NAME=$1
-    RESULT=$2    
-    padlength=40
-    pad=$(printf '%0.1s' "-"{1..60})
-    if [ "$RESULT" == "okey" ]; then
-        printf '%s' "$TEST_NAME"
-        printf '%*.*s' 0 $((padlength - ${#TEST_NAME} )) "$pad"
-        printf "[\e[1m\e[92mOK\e[0m]"  
-    else
-        printf '%s' "$TEST_NAME"
-        printf '%*.*s' 0 $((padlength - ${#TEST_NAME} )) "$pad"
-        printf "[\e[1m\e[31mFAIL\e[0m]"
-    fi
-}
+# function print_test {
+#     TEST_NAME=$1
+#     RESULT=$2    
+#     padlength=40
+#     pad=$(printf '%0.1s' "-"{1..60})
+#     if [ "$RESULT" == "okey" ]; then
+#         printf '%s' "$TEST_NAME"
+#         printf '%*.*s' 0 $((padlength - ${#TEST_NAME} )) "$pad"
+#         printf "[\e[1m\e[92mOK\e[0m]"  
+#     else
+#         printf '%s' "$TEST_NAME"
+#         printf '%*.*s' 0 $((padlength - ${#TEST_NAME} )) "$pad"
+#         printf "[\e[1m\e[31mFAIL\e[0m]"
+#     fi
+# }
 
 exec 3>&1 4>&2 1>"ntp-client-$HOSTNAME.log" 2>&1
 
