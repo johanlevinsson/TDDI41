c1071fb823596b9ce98b060de3e8e2cc538ba27e
Erik Peyronson <erikpeyronson@gmail.com>
2016-12-26 12:08:51 +0100

    test

--- scripts/test-cases/basic-network.sh
+++ scripts/test-cases/basic-network.sh
@@ -15,10 +15,11 @@ echo $HOSTNAME
 IP_ADRESS=$(ifconfig eth0 | awk '/inet addr/{print substr($2,6)}')
 echo "#$IP_ADRESS#"
 
-if [ "$IP_ADDRESS" == "130.236.178.219" ]; then
+if [ "$IP_ADRESS" == "130.236.178.219" ]; then
     if [ "$HOSTNAME" == "server" ]; then
 	EXTERNAL_ADRESS=$(ifconfig eth1 | awk '/inet addr/{print substr($2,6)}')
-	if [ "$EXTERNAL_ADDRESS" == "130.236.178.25" ]; then
+	echo "#$EXTERNAL_ADRESS#"
+	if [ "$EXTERNAL_ADRESS" == "130.236.178.25" ]; then
 	    TEST_RESULT=okey
 	else
 	    TEST_result=fail
