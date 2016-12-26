239e0c168666a6efa6931a2ed672d0be7706f4b8
Erik Peyronson <erikpeyronson@gmail.com>
2016-12-26 13:38:42 +0100

    tests

--- scripts/test-cases/master-router.sh
+++ scripts/test-cases/master-router.sh
@@ -1,10 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-# ID:
-# Purpose:
+# ID: 1
+# Purpose: Test all aspects from marsix
 # Preconditions
+# Lab series completed
 
 # Procedure:
 
 
+# exec 3>&1 4>&2 1>"name_resolution-$HOSTNAME.log" 2>&1
+# exec 1>&3 2>&4
+# echo >&2
+
+source /root/TDDI41/scripts/test-cases/basic-network.sh
+source /root/TDDI41/scripts/test-cases/quagga.sh
+source /root/TDDI41/scripts/test-cases/ntp-client.sh
+
+printf "Leaving router\n"
+
+
 # Expected results:
+
+# All tests [OK]
