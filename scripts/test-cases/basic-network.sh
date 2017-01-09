1eb7fdb5f5c58649d83c6b0ae02372cd7a52f801
Erik Peyronson <erikpeyronson@gmail.com>
2017-01-09 15:41:22 +0100

    asdfkljase

--- scripts/test-cases/basic-network.sh
+++ scripts/test-cases/basic-network.sh
@@ -33,11 +33,9 @@ HOSTNAME=$(hostname)
 
 #exec 3>&1 4>&2 1>"basic-network-$HOSTNAME.log" 2>&1
 IP_ADRESS=$(ifconfig eth0 | awk '/inet addr/{print substr($2,6)}')
-echo "#$IP_ADRESS#"
 
 if [ "$IP_ADRESS" == "130.236.178.218" ] && [ "$HOSTNAME" == "gw" ]; then
     EXTERNAL_ADRESS=$(ifconfig eth1 | awk '/inet addr/{print substr($2,6)}')
-    echo "#$EXTERNAL_ADRESS#"
     if [ "$EXTERNAL_ADRESS" == "130.236.178.25" ]; then
 	TEST_RESULT=okey
     else
@@ -47,13 +45,10 @@ if [ "$IP_ADRESS" == "130.236.178.218" ] && [ "$HOSTNAME" == "gw" ]; then
 else
     if [ "$IP_ADRESS" == "130.236.178.219" ] && [ "$HOSTNAME" == "server" ]; then
 	TEST_RESULT=okey
-	echo "eth0 ip: $IP_ADRESS hostname: $HOSTNAME\n"
     elif [ "$IP_ADRESS" == "130.236.178.220" ] && [ "$HOSTNAME" == "client-1" ]; then
 	TEST_RESULT=okey
-	echo "eth0 ip: $IP_ADRESS hostname: $HOSTNAME\n"
     elif [ "$IP_ADRESS" == "130.236.178.221" ] && [ "$HOSTNAME" == "client-2" ]; then
 	TEST_RESULT=okey
-	echo "eth0 ip: $IP_ADRESS hostname: $HOSTNAME\n"
     else
 	TEST_RESULT=fail
 	echo "Hostname ip addres on $HOSTNAME failed due to hostname-ip missmatch\n"
